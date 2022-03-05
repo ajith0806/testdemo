fc0209c6beceeb9260d099023c241d6900b4ba70
ajith <ajithkumar2766@gmail.com>
2022-03-05 13:39:10 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/*let a = "Ajith";
+let b = "Kumar";
+console.log(a.length+b.length);
+const car = {type:"Fiat", model:"500", color:"white"};
+console.log(car.type);*/
+let person = {
+    person1:{
+ firstName: "ajith",
+    lastName: "kumar",
+    age: 21,
+    eyeColor: "blue"
+  },
+     person2: {
+      firstName: "mohan",
+      lastName: "loosu",
+      age: 22,
+      eyeColor: "black"
+    },
+      person3:{
+         firstName: "mustak",
+         lastName: "MENTAL",
+          age:23,
+          eyeColor: "brown"
+      }
+
+    };
+console.log(person.person3);
